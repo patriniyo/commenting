b90417bc134a05963c7dbaab647d35bc1b0ace0e
Paatrick Niyomwungeri <patriniyo12@gmail.com>
2019-03-15 22:23:26 -0700

    Adds user class

---
+++ commentmanage.js
@@ -0,0 +1,34 @@
+import mysql from 'mysql';
+
+class user {
+  constructor() {
+    this.loggedIn = false;
+    this.lastLoggedInAt = null;
+  }
+  logIn() {
+    this.lastLoggedInAt = new Date();
+    this.loggedIn = true;
+  }
+  logOut() {
+    this.loggedIn = false
+  }
+  connectToDataBase() {
+    this.connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: ""
+    });
+  }
+}
+class moderator extends user {
+  constructor() {
+    super();
+  }
+
+}
+class admin extends moderator {
+  constructor() {
+    super();
+  }
+
+}
