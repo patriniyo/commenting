be87233ea225e6a205be349ae6c8cf16909b5e3d
Paatrick Niyomwungeri <patriniyo12@gmail.com>
2019-03-18 22:39:55 -0700

    Deletes comments

--- commentmanage.js
+++ commentmanage.js
@@ -31,6 +31,7 @@ class User {
     const sql = `INSERT INTO comment (author, timestamp, message) VALUES (${this.myId},'${timestamp}','${message}')`;
     this.connection.query(sql, (err, result) => {
       if (err) throw err;
+      console.log("The comment created");
     });
     this.closeConnection();
   }
@@ -45,34 +46,52 @@ class User {
     this.closeConnection();
   }
 
-  /*testUser(id) {
-    if (this.isNormalUser(id)) {
-      console.log("this is a normal user");
-    } else {
-      console.log("He is a normal user");
-    }
-  }*/
-
-  isNormalUser(id) {
+  editComment(commentId, message) {
     this.connectToDataBase();
-    const sql = `SELECT role FROM users WHERE users.user_id = ${id}`;
+    const sql = `UPDATE comment SET message = '${message}' WHERE comment_id = ${commentId} AND author = ${this.myId}`;
     this.connection.query(sql, (err, result) => {
       if (err) throw err;
-      if (result[0].role === 1) {
-        return true;
-      } else {
-        return false;
-      }
-      //console.log(result);
+      console.log("The comment edited");
+    });
+    this.closeConnection();
+  }
+
+  deleteComment(commentId) {
+    this.connectToDataBase();
+    const sql = `DELETE FROM comment WHERE comment_id = ${commentId} AND author = ${this.myId}`;
+    this.connection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("The comment deleted");
+    });
+    this.closeConnection();
+  }
+
+  getAllComments() {
+    this.connectToDataBase();
+    const sql = `SELECT * FROM comment WHERE author = ${this.myId}`;
+    this.connection.query(sql, (err, result) => {
+      if (err) throw err;
+      result.forEach(comment => {
+        console.log(comment.message);
+      });
     });
     this.closeConnection();
   }
 }
+
 class moderator extends User {
   constructor(id) {
     super(id);
   }
-
+  deleteComment(commentId) {
+    this.connectToDataBase();
+    const sql = `DELETE FROM comment WHERE comment_id = ${commentId}`;
+    this.connection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("The comment deleted");
+    });
+    this.closeConnection();
+  }
 }
 class Admin extends moderator {
   constructor(id) {
@@ -87,9 +106,24 @@ class Admin extends moderator {
     this.closeConnection();
   }
 
+  editComment(commentId, message) {
+    this.connectToDataBase();
+    const sql = `UPDATE comment SET message = '${message}' WHERE comment_id = ${commentId}`;
+    this.connection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log("The comment edited");
+    });
+    this.closeConnection();
+  }
+
 }
-const user = new User(5);
+const user = new User(3);
+user.getAllComments();
 const admin = new Admin(3);
-admin.createComment("As an admin I can confirmm that");
-//user.createComment("The moment js is a great tool ever");
-user.createReply(3, "I love him too, He is great forever");
+//admin.editComment(2, "This is edited by the admin!");
+admin.deleteComment(2);
+//user.editComment(3, "This is edited!");
+//user.deleteComment(3);
+//admin.createComment("As an admin I can confirmm that");
+//user.createComment("Another comment after a deletion");
+//user.createReply(3, "I love him too, He is great forever");
